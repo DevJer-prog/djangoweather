d13e097096e204574d260c18837db59a0a9882fa
Jérémy <83728929+DevJer-prog@users.noreply.github.com>
2021-12-15 10:39:55 +0100

    Update settings.py

--- weather/settings.py
+++ weather/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2)wm!#eginptq$_90hzhf6suxwh3u05_79#1jhdq!wr05)kc2+'
+# SECURITY WARNING: keep the secret key used in production secret! ON EXTERNAL FILE
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
